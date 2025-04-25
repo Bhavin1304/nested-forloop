08e75f0d7451931d16992eac0c7e5efcac517851
Bhavin1304 <bagulbhavin6@gmail.com>
2025-04-25 16:21:19 +0530

    Add files via upload

---
+++ nested loop/for loop/2ans.js
@@ -0,0 +1,11 @@
+// 2 Look at this series: 7, 10, 8, 11, 9, 12, ... What number should come next?
+
+let a=7
+let b=10
+
+for( let i=1; i<=4; i++){
+    document.writeln(a+" "+b);
+    a++;
+    b++;
+
+}
---
+++ nested loop/for loop/3ans.js
@@ -0,0 +1,11 @@
+// Look at this series: 36, 34, 30, 28, 24, ... What number should come next?
+
+let a=36
+let b=34
+
+for( let i=5; 5<=i; i--){
+    document.writeln(a+" "+b);
+    a++;
+    b--;
+
+}
